e597fa685f7eb876255a815e8ac1dcf8181d3b3d
adurga-ariqt <adurga@ariqt.com>
2022-11-16 14:31:39 +0530

    Add project files.

---
+++ UsingGenericsReturnParameterType/Generics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingGenericsReturnParameterType
+{
+    public  class Generics<T1>
+    {
+        private T1 value;
+        public Generics(T1 v)
+        {
+            value = v;
+        }
+        public T1 GetValue()
+        {
+            return value;
+        }
+    }
+}
---
+++ UsingGenericsReturnParameterType/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UsingGenericsReturnParameterType;
+
+Console.WriteLine("Hello, World!");
+var exo = new Generics<double>(24.56);
+var  G=exo.GetValue();
+Console.WriteLine("the value is:{0}\n",G);
+
